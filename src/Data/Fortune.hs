cc9d4ba097b40aa298938c123e82ef7fbb96260b
James Cook <mokus@deepbondi.net>
2012-03-21 21:07:42 -0400

    Ignore .dat files for fortune-friendliness

--- src/Data/Fortune.hs
+++ src/Data/Fortune.hs
@@ -101,14 +101,21 @@ traverseDir rec onFile = fix $ \search dir ->
                 else onFile path
      in concat <$> (mapM onItem =<< listDir dir)
 
+isIndexPath path = case takeExtension path of
+    ".ix"   -> True
+    ".dat"  -> True
+    _       -> False
+
 -- |List all the fortune files in a directory.  The 'Bool' value
 -- specifies whether to search subtrees as well.
 --
--- Any file which does not have \".ix\" extension will be reported
--- as a fortune file.
+-- Any file which does not have an extension of \".ix\" or \".dat\"
+-- will be reported as a fortune file (\".dat\" is not used by
+-- misfortune, but is ignored so that misfortune can share fortune
+-- databases with @fortune@).
 listFortuneFiles :: Bool -> FilePath -> IO [FilePath]
 listFortuneFiles rec = traverseDir rec onFile
-    where onFile path = return [ path | takeExtension path /= ".ix" ]
+    where onFile path = return [ path | not (isIndexPath path) ]
 
 -- |List all the fortune files in several directories.  Each directory
 -- will be searched by 'listFortuneFiles' (using the corresponding 'Bool' 
